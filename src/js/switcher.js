df490c412ab46e109c80c6763625a0b99b0503a7
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-04-30 12:16:23 +0300

    fix dark theme and switcher

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,11 +1,15 @@
 const themeToggle = document.getElementById('theme-toggle');
-// Перевірити чи є вже збережена тема
+
+// Перевірка при завантаженні
 if (localStorage.getItem('theme') === 'dark') {
   document.body.classList.add('dark');
   document.body.classList.remove('light');
   themeToggle.checked = true;
+} else {
+  document.body.classList.add('light');
 }
 
+// Зміна теми
 themeToggle.addEventListener('change', () => {
   if (themeToggle.checked) {
     document.body.classList.add('dark');
